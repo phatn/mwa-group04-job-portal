88776c2802986a84edb1efe7d998823a6640d07a
Phat Nguyen <phatnguyentanit@gmail.com>
2022-08-13 15:09:03 -0500

    Updated title

--- frontend/server.js
+++ frontend/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist', 'frontend')));
+app.use(express.static(path.join(__dirname, 'dist', 'job-portal')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'frontend', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'job-portal', 'index.html'));
 })
 
 let port = process.env.PORT || 4200;
